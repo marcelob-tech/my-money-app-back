c590d710da4acd4e2f9f89eee111ffdaabd08b9f
marcelo-betassa <3986286+marcelob-tech@users.noreply.github.com>
2022-05-25 00:33:46 -0300

    auth back-end

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,11 +1,26 @@
+const auth = require('./auth');
 const express = require('express');
 
-module.exports = function(server) {
-    //Definir  URL base para todas as rotas
-    const router = express.Router();
-    server.use('/api', router);
+module.exports = function (server) {
+	/*
+	 * Rotas protegidas por Token JWT
+	 */
+	const protectedApi = express.Router();
+	server.use('/api', protectedApi);
+	protectedApi.use(auth);
 
-    // Rotas do ciclo de pagamento
-    const BillingCycle = require('../api/billingCycle/billingCycleService');
-    BillingCycle.register(router, '/billingCycles');
-}
+	// Rotas de Ciclo de Pagamento
+	const BillingCycle = require('../api/billingCycle/billingCycleService');
+	BillingCycle.register(protectedApi, '/billingCycles');
+
+	/*
+	 * Rotas abertas
+	 */
+	const openApi = express.Router();
+	server.use('/oapi', openApi);
+
+	const AuthService = require('../api/user/AuthService');
+	openApi.post('/login', AuthService.login);
+	openApi.post('/signup', AuthService.signup);
+	openApi.post('/validateToken', AuthService.validateToken);
+};
